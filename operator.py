c013a6a4373ee82a5c97eb8119e77f986f93865f
davidselvam <smilingdavid001@gmail.com>
2024-03-06 16:27:33 +0530

    Edited : added assignment operation

--- operator.py
+++ operator.py
@@ -8,3 +8,21 @@ print(f"multiplication:{a*b}")
 print(f"division:{a/b}")
 print(f"floor division:{a//b}")
 print(f"Exponation:{a**b}")
+
+# Assignment operators
+
+x = 5
+x += 2
+print({x})
+x -= 1
+print(x)
+x *= 2
+print(x)
+x /= 2
+print(x)
+x //= 2
+print(x)
+x **= 2
+print(x)
+x %= 2
+print(x)
