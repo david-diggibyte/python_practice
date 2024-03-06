236e3c02a4d7fff7024c2b85dbccc25f38124577
davidselvam <smilingdavid001@gmail.com>
2024-03-06 16:14:07 +0530

    Added : added arithmetic in operator

---
+++ operator.py
@@ -0,0 +1,10 @@
+# operator
+# Arithmetic operators
+a = 20
+b = 10
+print(f"addition:{a+b}")
+print(f"subtraction:{a-b}")
+print(f"multiplication:{a*b}")
+print(f"division:{a/b}")
+print(f"floor division:{a//b}")
+print(f"Exponation:{a**b}")
