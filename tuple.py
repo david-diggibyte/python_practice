eb7559c9cf58a6238c96ae6f3f0771f0edc343d2
davidselvam <smilingdavid001@gmail.com>
2024-03-07 16:30:19 +0530

    added : working tuple and build-in function

---
+++ tuple.py
@@ -0,0 +1,9 @@
+# tuple and build-in function
+tuple1 = (44,33,22,44,5,6,87,9,55,443,767,32,76,9)
+index = tuple1.index(22) # return the index
+print(index)
+count = tuple1.count(44) # return the count
+print(count)
+print(sorted(tuple1)) # sorting the tuple using function
+print(min(tuple1)) # return the minimum value
+print(max(tuple1)) # return the maximum valiue
