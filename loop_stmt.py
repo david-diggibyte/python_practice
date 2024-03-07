441e4aec7d3bff44a4a35088b01ceabde0e24cbb
davidselvam <smilingdavid001@gmail.com>
2024-03-07 11:36:38 +0530

    Edited :iterate number using while loop

--- loop_stmt.py
+++ loop_stmt.py
@@ -11,3 +11,9 @@ for i in range(0,20,2):
 user = {"name":"david","age":22,"gender":"male","degree":"bsc","native":"kallakuruchi"}
 for i,j in user.items():
     print(i,j)
+# While loop
+n = int(input("Enter a number:"))
+count = 0
+while count <= n:
+    print(count)
+    count +=1
