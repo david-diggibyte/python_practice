cc2ec7c827cef71b95c8aeabc42b899f19a8d95c
davidselvam <smilingdavid001@gmail.com>
2024-03-07 11:08:52 +0530

    Edited : using range function in for loop

--- loop_stmt.py
+++ loop_stmt.py
@@ -1,5 +1,8 @@
 # looping statement
-
+#iterate list using for loop"
 x = ["david","susai","sasi","chandrhu","seelan","akilan"]
 for i in x:
+    print(i)
+# using range function in for loop
+for i in range(0,20,2):
     print(i)
