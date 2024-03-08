4f88a3619de88d3880ce6a0a302b8ba8f8d75fc9
davidselvam <smilingdavid001@gmail.com>
2024-03-08 12:48:00 +0530

    Edited : find palidrome using function

--- function.py
+++ function.py
@@ -8,6 +8,15 @@ def anagram(name1,name2):
    else:
         print("not anagram ..")
 word1 = str(input("Enter a name1 : "))
-word2 = str(input(("Enter a name2 :")))
-anagram (name1 = word1,name2 = word2)
+word2 = str(input(("Enter a name2 : ")))
+anagram (name1 = word1,name2 = word2)  # called tha function
 
+# find palindrome or not
+
+def palindrome(word):
+    if word == word[::-1]:
+        print("palinrome !!")
+    else:
+        print("not palindrome ..")
+name = str(input("Enter a name :"))
+palindrome(word = name) # called tha function
