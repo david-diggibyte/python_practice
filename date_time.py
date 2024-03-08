52171ffda1620864ba2d14d18973a15889e2ac3a
davidselvam <smilingdavid001@gmail.com>
2024-03-09 03:06:18 +0530

    added : datetime module used

---
+++ date_time.py
@@ -0,0 +1,27 @@
+# dates ,times ,deltas and times zones
+
+import datetime as dt
+# print(dt.datetime.now()) # print currend time and date
+# print(dt.datetime.now().date()) # print time date
+# print(dt.date.today()) # print date only
+# print(dt.datetime.now().time()) # print time only
+
+# # date delta
+
+# date1 = dt.date.today()
+# date2 = dt.timedelta(days=4)
+# print(date1)
+# print(date1 - date2) # difference
+
+# date1 = dt.date(2024,1,1)
+# date2 = dt.datetime.now().date()
+# print(date2 - date1)
+
+# time zone
+
+import pytz
+
+dt1 = dt.datetime.now()
+print(dt1)
+dt2 = dt.timezone.now(pytz.utc)
+print(dt2)
