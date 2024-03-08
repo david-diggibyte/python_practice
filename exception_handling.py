5ea8db9a4d348b57c75200ff04ce01989ed13f77
davidselvam <smilingdavid001@gmail.com>
2024-03-09 02:14:06 +0530

    Added : handling error by try,catch,finally

---
+++ exception_handling.py
@@ -0,0 +1,13 @@
+# zero by division handlig
+
+try:
+    a = int(input("enter a number 1 :"))
+    b = int(input("enter a number 2 :"))
+    c = a / b
+    print(c)
+except Exception as e:
+    print(f"{e} error")
+except Exception as e:
+    print(f"{e} error")
+finally:
+    print("complited")
