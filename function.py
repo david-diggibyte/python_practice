9ccfeeba75f1c6f9490d9c5ade13ce88332e72da
davidselvam <smilingdavid001@gmail.com>
2024-03-08 12:40:47 +0530

    Added : find anagram or not using function

---
+++ function.py
@@ -0,0 +1,13 @@
+# practice function
+
+# find anagram using function
+
+def anagram(name1,name2):
+   if sorted(name1) == sorted(name2):
+        print("anagram !!")
+   else:
+        print("not anagram ..")
+word1 = str(input("Enter a name1 : "))
+word2 = str(input(("Enter a name2 :")))
+anagram (name1 = word1,name2 = word2)
+
