4fc5878eb3754739e83cb89a405392ede6043e58
davidselvam <smilingdavid001@gmail.com>
2024-03-09 18:11:53 +0530

    Added : added one class and object example

--- class_object.py
+++ class_object.py
@@ -18,3 +18,22 @@ k7.display()
 
 dhinakaran = Employee("dhinakaran","Data Engineer",10000)
 dhinakaran.display()
+
+# another example
+
+class Fruits(): # class created
+    def __init__(self,fruit,colour,quantity):
+        self.fruit = fruit
+        self.quantity = quantity
+        self.colour = colour
+    def show(self):
+        return f"fruit name is {self.fruit} colour is {self.colour} and quantity is {self.quantity}"
+
+apple = Fruits("apple","red",12) # object created
+print(apple.show())
+
+orange = Fruits("Orange","yellow",10)
+print(orange.show())
+
+banana = Fruits("Banana","yellow",5)
+print(banana.show())
