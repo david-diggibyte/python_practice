90951cda142bbfef49d8060b147f6c5d02d8c579
davidselvam <smilingdavid001@gmail.com>
2024-03-09 12:30:42 +0530

    Added : time zone

--- date_time.py
+++ date_time.py
@@ -1,21 +1,21 @@
 # dates ,times ,deltas and times zones
 
 import datetime as dt
-# print(dt.datetime.now()) # print currend time and date
-# print(dt.datetime.now().date()) # print time date
-# print(dt.date.today()) # print date only
-# print(dt.datetime.now().time()) # print time only
+print(dt.datetime.now()) # print currend time and date
+print(dt.datetime.now().date()) # print time date
+print(dt.date.today()) # print date only
+print(dt.datetime.now().time()) # print time only
 
-# # date delta
+# date delta
 
-# date1 = dt.date.today()
-# date2 = dt.timedelta(days=4)
-# print(date1)
-# print(date1 - date2) # difference
+date1 = dt.date.today()
+date2 = dt.timedelta(days=4)
+print(date1)
+print(date1 - date2) # difference
 
-# date1 = dt.date(2024,1,1)
-# date2 = dt.datetime.now().date()
-# print(date2 - date1)
+date1 = dt.date(2024,1,1)
+date2 = dt.datetime.now().date()
+print(date2 - date1)
 
 # time zone
 
@@ -25,3 +25,11 @@ dt1 = dt.datetime.now()
 print(dt1)
 dt2 = dt.timezone.now(pytz.utc)
 print(dt2)
+
+
+
+# Get the current time in Sweden
+
+sweden_timezone = pytz.timezone('Europe/Stockholm')
+sweden_time = dt.now(sweden_timezone)
+print(sweden_time)
