2ed7ef2f10c30a32bc86a7ecec6603267e99c044
davidselvam <smilingdavid001@gmail.com>
2024-03-11 14:17:59 +0530

    Edited : practice overriding

--- polymorphism.py
+++ polymorphism.py
@@ -1,40 +1,64 @@
 # Polymorphisam
+#
+# print(type("david"))
+# print(type(555))
+#
+# class Addition():
+#     def __init__(self,a,b,c = 0):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#     def add(self):
+#         return self.a + self.b + self.c
+#
+# sum1 = Addition(30,50)
+# print(sum1.add())
+#
+# sum2 = Addition(50,50,50)
+# print(sum2.add())
+#
+# sum3 = Addition(2,2,2)
+# print(sum3.add())
+#
+# # Method overloading another example
+#
+# class Multiplication():
+#     total = 1
+#     def mul(self,*args):
+#        total = 1
+#        for i in args:
+#         total *=  i
+#        return total
+#
+# mul1 = Multiplication()
+# print(mul1.mul(5,5))
+#
+# mul2 = Multiplication()
+# print(mul2.mul(1,2,3,4,5))
+#
+# mul3 = Multiplication()
+# print(mul3.mul(100,100,100))
 
-print(type("david"))
-print(type(555))
 
-class Addition():
-    def __init__(self,a,b,c = 0):
-        self.a = a
-        self.b = b
-        self.c = c
-    def add(self):
-        return self.a + self.b + self.c
+#  method overriding
+# method overriding  is a function have same name and same parameter.
 
-sum1 = Addition(30,50)
-print(sum1.add())
+class Tutorial():
+    def __init__(self,name):
+        self.name = name
+    def show(self):
+        return f"{self.name} institute "
+class course(Tutorial):
+    def __init__(self,subject):
+        self.subject = subject
+    def show(self):
+        return f"{self.subject} course "
 
-sum2 = Addition(50,50,50)
-print(sum2.add())
+sla = Tutorial("SLA")
+print(sla.show())
 
-sum3 = Addition(2,2,2)
-print(sum3.add())
+python = course("python")
+print(python.show())
 
-# Method overloading another example
-
-class Multiplication():
-    total = 1
-    def mul(self,*args):
-       total = 1
-       for i in args:
-        total *=  i
-       return total
-
-mul1 = Multiplication()
-print(mul1.mul(5,5))
-
-mul2 = Multiplication()
-print(mul2.mul(1,2,3,4,5))
-
-mul3 = Multiplication()
-print(mul3.mul(100,100,100))
+java = course("java")
+print(java.show())
