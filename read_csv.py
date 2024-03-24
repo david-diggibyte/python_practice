29dc31363391af8b1a0317befd47c18285b7735d
davidselvam <smilingdavid001@gmail.com>
2024-03-25 02:59:34 +0530

    Adding to read CSV file using format function

--- read_csv.py
+++ read_csv.py
@@ -9,4 +9,8 @@ df.show()
 df.printSchema()
 
 
+# reading the csv file using format function
 
+df1 = spark.read.format('csv').option(key='header',value=True).option(key='inferSchema',value=True).load(path=r'C:\Users\AKASH.S\Documents\data.csv')
+df1.show()
+df1.printSchema()
